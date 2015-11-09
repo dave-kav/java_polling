7fa09e4df8273591b3f6c33766809d45ee1251f6
Dave Kavanagh <dave.kavanagh87@gmail.com>
2015-11-09 10:56:21 +0000

    changed bat timeouts

--- src/start.bat
+++ src/start.bat
@@ -1,9 +1,9 @@
 javac polling/*.java
-timeout /t 5
+timeout /t 2
 rmic polling.PollFactoryImpl
-timeout /t 5
+timeout /t 2
 start rmiregistry 2000
-timeout /t 5
+timeout /t 2
 start java -Djava.security.policy=polling/policy.txt polling.PollServer 127.0.0.1:2000
-timeout /t 5
+timeout /t 2
 java -Djava.security.policy=polling/policy.txt polling.PollClient 127.0.0.1:2000
